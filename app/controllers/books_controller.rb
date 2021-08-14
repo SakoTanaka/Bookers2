e831ae2ef257e95a5b3ccb18d4d5b37dedc56bf3
SakoTanaka <tanaka.sako@gmail.com>
2021-08-14 15:04:57 +0000

    [Start]controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,15 @@
 class BooksController < ApplicationController
 
-  def new
+  def show
+   @newbook = Book.new
+   @book = Book.find(params[:id])
+   @user = @book.user
+  end
+
+  def index
    @book = Book.new
+   @books = Book.all
+   @user = current_user
   end
 
   def create
@@ -9,40 +17,39 @@ class BooksController < ApplicationController
    @book.user_id = current_user.id
   if @book.save
    flash[:notice] = "Book was successfully created."
-   redirect_to "/users"
+   redirect_to book_path(@book)
   else
+   @user = current_user
    @books = Book.all
    render:show
   end
   end
 
-  def index
-   @books = Book.all
-  end
-
-  def show
-   @book = Book.find(params[:id])
-  end
-
   def edit
-  @book = Book.find(params[:id])
+   @book = Book.find(params[:id])
+  if @book.user = current_user
+   render "edit"
+  else
+   redirect_to books_path
   end
 
   def update
    @book = Book.find(params[:id])
+   @book.user_id = current_user
   if @book.update(book_params)
    flash[:notice] = "Book was successfully updated."
-   redirect_to @book
+   redirect_to book_path(@book.id)
   else
    render:edit
   end
   end
 
   def destroy
-  book = Book.find(params[:id])
-  book.destroy
-  flash[:notice] = "Book was successfully destroyed."
-  redirect_to books_path
+   @book = Book.find(params[:id])
+   if @book.destroy
+   flash[:notice] = "Book was successfully destroyed."
+   redirect_to books_path
+  end
   end
 
 private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,34 @@
 class UsersController < ApplicationController
 
-  def index
-    @users = User.all
-  end
-
   def show
     @user = User.find(params[:id])
+    @book = Book.new
+    @books = Book.where(user_id: @user.id)
+  end
+
+  def index
+    @user = current_user
+    @users = User.all
+    @book = Book.new
+    @books = Book.all
   end
 
   def edit
     @user = User.find(params[:id])
+    if @user = current_user
+       render"edit"
+    else
+      redirect_to user_path(current_user)
   end
 
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
-    redirect_to user_path(@user.id)
+    flash[:notice]="You have updated user successfully."
+    redirect_to user_path(current_user)
+  else
+    render :edit
+  end
   end
 
   private
